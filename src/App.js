49ca10d86cb993de0d6f3ff8649c917f732fbaa5
Daniela Gonzalez <d.gonzalezc1@uniandes.edu.co>
2022-09-13 16:50:20 -0500

    Change in App name and logo, creation of Home and dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { IntlProvider } from "react-intl";
+import { useState, useEffect } from "react";
+import en from "./Dictionaries/en.json";
+import es from "./Dictionaries/es.json";
+import Home from './Pages/Home/Home';
 
 function App() {
+
+  // stablish default language at navigator's storage (if not previously existent)
+  let storedLanguage = localStorage.getItem('language');
+  if (!storedLanguage) {
+    localStorage.setItem('language', navigator.language);
+    storedLanguage = navigator.language;
+  }
+
+  const [languageSettings, setLanguageSettings] = useState({ messages: storedLanguage.startsWith("es") ? es : en, locale: storedLanguage });
+
+  function setLang(lang) {
+    const updatedLang = { messages: undefined, locale: lang };
+    if (lang.startsWith(navigator.language.slice(0, 2))) {
+      updatedLang.locale = navigator.language;
+    }
+    updatedLang.messages = updatedLang.locale.startsWith("es") ? es : en;
+    localStorage.setItem("language", updatedLang.locale);
+    setLanguageSettings(updatedLang);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IntlProvider
+          locale={languageSettings.locale}
+          messages={languageSettings.messages}
+      >
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+            </Routes>
+          </BrowserRouter>
+        </IntlProvider>
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,82 @@
+import "./Home.css";
+import React, {useLayoutEffect} from 'react';
+import {FormattedMessage} from "react-intl";
+import {Carousel} from "react-bootstrap";
+import Navbar from "../../Components/Navbar/Navbar";
+
+function Home() {
+
+    useLayoutEffect(()=>{
+        window.scrollTo(0,0)
+    })
+
+
+  return (
+      <React.Fragment>
+          <Navbar/>
+          <div id="AboutUs">
+              <div className="container-fluid">
+                  <div className="aboutUs-banner row">
+                      <div className="col-12 aboutUs-banner-col">
+                          <img className="aboutUs-banner-img" src="/Assets/Photos/Home/Banner.png" alt="Banner artisan" />
+                          <div id="aboutUs-banner-helper">
+                              <h1><FormattedMessage id="TitleAboutUs"/></h1>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="aboutUs-motivation row align-items-center">
+                      <div className="col-12 col-lg-6">
+                          <h2>
+                              <FormattedMessage id="SubtitleAboutUs" values={{
+                                  span: (chunks) => <span className='text-italic'>{chunks}</span>
+                              }}/>
+                          </h2>
+                      </div>
+                      <div className="col-12 col-lg-6 center-content">
+                          <p><FormattedMessage id="ParagraphDream1"/></p>
+                          <p><FormattedMessage id="ParagraphDream2"/></p>
+                      </div>
+                  </div>
+                  <div className="aboutUs-artisans row align-items-center">
+                      <div className="col-12 d-block d-lg-none">
+                          <img id="aboutUs-artisans-small-image" src="/Assets/Photos/Home/Mockup.png" alt="Mockups"/>
+                      </div>
+                      <div className="aboutUs-artisans-txt col-12 col-lg-5">
+                          <h3>
+                              <FormattedMessage id="WhoAreTheArtisans" values={{
+                                  span: (chunks) => <span className="text-italic">{chunks}</span>
+                              }}/>
+                          </h3>
+                          <p><FormattedMessage id="ArtisansExplanation1"/></p>
+                          <p><FormattedMessage id="ArtisansExplanation2"/></p>
+                          <p className='text-bold'><FormattedMessage id="ArtisansExplanation3"/></p>
+                      </div>
+                      <div className="col-lg-7 d-none d-lg-block text-center ">
+                          <img src="/Assets/Photos/Home/Mockup.png" alt="Artisan" className="aboutUs-artisans-img"/>
+                      </div>
+                  </div>
+                  <div className="aboutUs-behindITTI row">
+                      <div className="col-12">
+                          <h3 id="aboutUs-WhoAreWe">
+                              <FormattedMessage id="WhoAreWeBehindITTI"/>
+                          </h3>
+                      </div>
+
+                      <div className="col-12 col-lg-6 text-center">
+                          <img id="team1imageSmall" src="/Assets/Photos/Home/Team1.png" alt="ITTI Team #1" className="d-block d-lg-none"/>
+                          <p><FormattedMessage id="TeamITTI"/></p>
+                          <img id="team1image" src="/Assets/Photos/Home/Team1.png" alt="ITTI Team #1" className="d-none d-lg-block"/>
+                      </div>
+                      <div className="col-lg-6 text-center aboutUs-team2-col d-none d-lg-block">
+                          <img id="team2image" src="/Assets/Photos/Home/Team2.png" alt="ITTI Team #2"/>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+      </React.Fragment>
+
+  )
+}
+
+export default Home
