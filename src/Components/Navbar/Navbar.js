33a7ce936db832c3fbaeb08c545043a2fd89481c
Juan David Serrano <jd.serranom@uniandes.edu.co>
2022-09-14 07:23:02 -0500

    Added elements to the dictionary

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div id="Navbar" className="container-fluid">
+      <nav className="navbar navbar-expand-sm navbar-light">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   )
 }
 
